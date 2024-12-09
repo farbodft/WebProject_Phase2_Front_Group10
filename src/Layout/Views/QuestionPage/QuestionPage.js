7b1687b6e23e4faf67c57186ea411d60576ecce6
farbodft <farbod.ft2005@gmail.com>
2024-12-09 18:07:29 +0330

    navbar for QuestionPage.js updated

--- src/Layout/Views/QuestionPage/QuestionPage.js
+++ src/Layout/Views/QuestionPage/QuestionPage.js
@@ -2,25 +2,14 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import QuestionContent from '../../Components/QuestionContent/QuestionContent';
 import Footer from '../../Components/Footer/Footer';
+import Navbar from "../../Components/Navbar/Navbar";
 
 const QuestionPage = () => {
     const navigate = useNavigate();
 
     return (
         <div>
-            <nav className="navbar">
-                <button onClick={() => navigate('/')} className="navbar-btn">خروج</button>
-                <div className="right-section">
-                    <div className="nav-links">
-                        <a href="/QuestionBox">مدیریت سوالات</a>
-                        <a href="/ScoreTable">جدول امتیازات</a>
-                        <a href="/MainPlayerPage">صفحه اصلی</a>
-                    </div>
-                    <div className="logo">
-                        <img src="/photo/logo.png" alt="Logo" />
-                    </div>
-                </div>
-            </nav>
+            <Navbar usage={"Player"} />
             <QuestionContent />
             <Footer />
         </div>
