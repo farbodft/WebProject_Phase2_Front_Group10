36a0f0f9315b305d5f13ceb7e93ce3e1cb93d0c3
mahsahajirahimi <mahsahajirahimi2003@gmail.com>
2024-12-13 02:08:37 -0800

    back and front are now connected

--- src/Layout/Components/LoginForm/LoginForm.js
+++ src/Layout/Components/LoginForm/LoginForm.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginForm.css';
 
-const LoginForm = ({ onSubmit }) => {
+const LoginForm = ({ onSubmit, error }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit({ username, password });
+    };
+
     return (
         <div className="form-box">
-            <form onSubmit={onSubmit}>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="username-login">نام کاربری:</label>
-                <input type="text" id="username-login" name="username-login" />
+                <input
+                    type="text"
+                    id="username-login"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
                 <label htmlFor="password-login">گذرواژه:</label>
-                <input type="password" id="password-login" name="password-login" />
+                <input
+                    type="password"
+                    id="password-login"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                {error && <div style={{ color: 'red', marginTop: '5px' }}>{error}</div>}
                 <button type="submit">ورود</button>
             </form>
         </div>
@@ -16,4 +39,3 @@ const LoginForm = ({ onSubmit }) => {
 };
 
 export default LoginForm;
-
